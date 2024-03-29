3f3b9148b1e2de6210abc2fb178bc49dccc6e3ff
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-03-29 18:35:37 +0100

    main

--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from './user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-const root = document.getElementById('root');
-
-const div = document.creareElement('div');
-div.textContant = 'What are you doing man?';
-div.id = '4040';
-
-root.append(div);
