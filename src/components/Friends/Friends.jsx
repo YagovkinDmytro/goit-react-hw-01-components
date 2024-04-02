3cebef45dea858b6538b4a581ed7c6dd115707ef
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-04-02 21:17:18 +0200

    friends style

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,10 +1,21 @@
+import friendsStyle from './friends.module.css';
+
 const FriendList = ({ friends }) => {
   const friendsListItem = friends =>
     friends.map(({ id, isOnline, avatar, name }) => (
-      <li className="item" key={id}>
-        <span className="status"></span>
-        <img className="avatar" src={avatar} alt="User avatar" width="48" />
-        <p className="name">{name}</p>
+      <li className={friendsStyle.item} key={id}>
+        <span
+          className={`${friendsStyle.status} ${
+            friendsStyle[`${isOnline ? 'online' : 'offline'}`]
+          }`}
+        ></span>
+        <img
+          className={friendsStyle.avatar}
+          src={avatar}
+          alt="User avatar"
+          width="48"
+        />
+        <p className={friendsStyle.name}>{name}</p>
       </li>
     ));
 
