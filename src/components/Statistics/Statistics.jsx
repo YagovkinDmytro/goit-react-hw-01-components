06a37e866c963ba5a72759dc14980e0d6ea5c418
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-04-02 19:44:32 +0200

    statistics style

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,5 @@
+import statisticsStyle from './statistics.module.css';
+
 const Statistics = ({ title, stats }) => {
   // const markupListItem = stats =>
   //   stats.map(({ id, label, percentage }) => (
@@ -6,16 +8,28 @@ const Statistics = ({ title, stats }) => {
   //       <span className="percentage">{percentage}</span>
   //     </li>
   //   ));
+
+  const getRandomHexColor = function () {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  };
   return (
-    <section className="statistics">
-      {{ title } && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">
-        {stats.map(({ id, label, percentage }) => (
-          <li className="item" key={id}>
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}</span>
-          </li>
-        ))}
+    <section className={statisticsStyle.statistics}>
+      {title && <h2 className={statisticsStyle.title}>{title}</h2>}
+      <ul className={statisticsStyle['stat-list']}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              style={{ backgroundColor: getRandomHexColor() }}
+              className={statisticsStyle.item}
+              key={id}
+            >
+              <span className={statisticsStyle.label}>{label}</span>
+              <span className={statisticsStyle.percentage}>{percentage}</span>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
